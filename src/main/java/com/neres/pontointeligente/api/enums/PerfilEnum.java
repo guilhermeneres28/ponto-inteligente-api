723b37f7f4fb030512a66b1ff3fafdc7d1ceed1e
Guilherme Neres <guilherme.neres.henrique@gmail.com>
2018-02-16 19:42:58 -0200

    Adicionado as entidades do sistema

---
+++ src/main/java/com/neres/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.neres.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO;
+}
---
+++ src/main/java/com/neres/pontointeligente/api/model/Empresa.java
@@ -0,0 +1,10 @@
+package com.neres.pontointeligente.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table
+public class Empresa implements Serializable {
+}
